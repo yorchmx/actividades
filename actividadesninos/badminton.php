b12e64af9b54f7101a0eb59e7c994a03f27ba13c
Jorge Manrique <58120477+yorchmx@users.noreply.github.com>
2023-03-24 11:30:36 -0600

    Ficha de pagos Bádminton

--- actividadesninos/badminton.php
+++ actividadesninos/badminton.php
@@ -1,32 +1,42 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/style.css">
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:ital,wght@0,100;0,200;0,300;0,400;1,100;1,200&display=swap" rel="stylesheet"><link href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:ital,wght@0,100;0,200;0,300;0,400;1,100;1,200&display=swap" rel="stylesheet">
-    <title>Bádminton</title>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Cards</title>
+	<link rel="stylesheet" href="../css/estilos.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
+
 <body>
-   
-    <header class="header">
-        <nav class="navbar">
-            <a href="#">Inicio</a>
-            <a href="#">About</a>
-            <a href="#">Services</a>
-            <a href="#">Contact</a>    
-        </nav>
 
-        <form action="#" class="search-bar">
-            <input type="text" placeholder="Search...">
-            <button type="submit"><i class='bx bx-search'></i></button>
-        </form>
+	<div class="container">
 
-    </header>
+		<div class="card">
+			<div class="left-column background1-left-column">
+				<h6>Control de versiones</h6>
+				<h2>GitHub</h2>
+				<i class="fa fa-github"></i>
+			</div>
 
-    <div class="container"></div>
-    
+			<div class="right-column">
+				<div>
+					<h4>Dificultad</h4>
+					<h6>Baja - Media</h6>
+
+				</div>
+				<h2>Aprende GitHub en 3 semanas</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, sintLorem ipsum dolor sit amet,
+					consectetur adipisicing elit. Harum, sint??</p>
+				<button class="button background1-left-column">Empezar</button>
+			</div>
+
+		</div>
+
+		
+	</div>
 </body>
+
 </html>
--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Academias Infantiles Club France</title>
-    <!--<link rel="stylesheet" href="css/style.css">-->
+    <link rel="stylesheet" href="css/style.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500&display=swap" rel="stylesheet">
 
     <!-- Bootstrap core CSS -->
