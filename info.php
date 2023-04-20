74e11ca94c919b7dc8a08b162d54111acc4652aa
Jorge Manrique <58120477+yorchmx@users.noreply.github.com>
2023-04-19 19:31:46 -0600

    Info de actividades deportivas

---
+++ info.php
@@ -0,0 +1,134 @@
+<?php
+
+
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Info</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:ital,wght@0,100;0,200;0,300;0,400;1,100;1,200&family=Montserrat:ital,wght@0,100;0,200;0,400;0,500;1,200;1,300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+  </head>
+
+  <body>
+    <br>
+    <div class="container">
+    <h1 class="titulo text-center">Actividades Deportivas</h1>
+    </div>
+    
+    <div class="container principal">
+    <div class="row">
+        <div class="col-lg-12" text-center>
+            <div class="row">
+                <!-- Tarjeta 1 -->
+                <div class="col-lg-4 col-md-12 mb-4">
+                    <div class="card-section border rounded p-3">
+                        <div class="card-header-first rounded pb-5">
+                        <h2 class="card-header-title text-white pt-3">Bádminton</h2>
+                        </div>
+                        <br>
+                    <div class="card-body text-center">
+                        <p class="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                            when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
+                            <br>
+                            <button class="btn btn-primary">Comprar</button>
+                        </div>
+                    </div>
+                    <br>
+                </div>
+                <!-- Tarjeta 2 -->
+                <div class="col-lg-4 col-md-12 mb-4">
+                    <div class="card-section border rounded p-3">
+                        <div class="card-header-first rounded pb-5">
+                        <h2 class="card-header-title text-white pt-3">Ballet</h2>
+                        </div>
+                        <br>
+                    <div class="card-body text-center">
+                        <p class="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                            when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
+                            <br>
+                            <button class="btn btn-primary">Comprar</button>
+                        </div>
+                    </div>
+                    <br>
+                </div>
+                <br>
+                <!-- Tarjeta 3 -->
+                <div class="col-lg-4 col-md-12 mb-4">
+                    <div class="card-section border rounded p-3">
+                        <div class="card-header-first rounded pb-5">
+                        <h2 class="card-header-title text-white pt-3">Básquetbol Infantil</h2>
+                        </div>
+                        <br>
+                    <div class="card-body text-center">
+                        <p class="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                            when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
+                            <br>
+                            <button class="btn btn-primary">Comprar</button>
+                        </div>
+                    </div>
+                    <br>
+                </div>
+                <!-- Tarjeta 4 -->
+                <div class="col-lg-4 col-md-12 mb-4">
+                    <div class="card-section border rounded p-3">
+                        <div class="card-header-first rounded pb-5">
+                        <h2 class="card-header-title text-white pt-3">Box Infantil</h2>
+                        </div>
+                        <br>
+                    <div class="card-body text-center">
+                        <p class="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                            when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
+                            <br>
+                            <button class="btn btn-primary">Comprar</button>
+                        </div>
+                    </div>
+                    <br>
+                </div>
+                <br>
+                <!-- Tarjeta 5 -->
+                <div class="col-lg-4 col-md-12 mb-4">
+                    <div class="card-section border rounded p-3">
+                        <div class="card-header-first rounded pb-5">
+                        <h2 class="card-header-title text-white pt-3">Box Infantil</h2>
+                        </div>
+                        <br>
+                    <div class="card-body text-center">
+                        <p class="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                            when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
+                            <br>
+                            <button class="btn btn-primary">Comprar</button>
+                        </div>
+                    </div>
+                    <br>
+                </div>
+                <!-- Tarjeta 6 -->
+                <div class="col-lg-4 col-md-12 mb-4">
+                    <div class="card-section border rounded p-3">
+                        <div class="card-header-first rounded pb-5">
+                        <h2 class="card-header-title text-white pt-3">Box Infantil</h2>
+                        </div>
+                        <br>
+                    <div class="card-body text-center">
+                        <p class="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                            when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
+                            <br>
+                            <button class="btn btn-primary">Comprar</button>
+                        </div>
+                    </div>
+                    <br>
+                </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+ </body>
+</html>
