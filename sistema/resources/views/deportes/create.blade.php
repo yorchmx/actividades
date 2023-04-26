31581a64a84056f039e8d7dc5b6a03d93f360db6
Jorge Manrique <58120477+yorchmx@users.noreply.github.com>
2023-04-26 14:22:35 -0600

    Sistema

--- sistema/resources/views/deportes/create.blade.php
+++ sistema/resources/views/deportes/create.blade.php
@@ -2,26 +2,30 @@ Formulario de creacion de empleados
 
 <form action="" method="post" encrypte="multipart/form-data" >
 
-    <input type="text" name="Actividad">
-    <input type="text" name="Edad"> 
-    <input type="text" name="Edad">
-    <input type="text" name="Edad">
-    <input type="text" name="Edad">
-    <input type="text" name="Edad">
-    <input type="text" name="Edad">
-    <input type="text" name="Edad">
-    <input type="text" name="Edad">
-    <input type="text" name="Edad">
-    <input type="text" name="Edad">
-    <input type="text" name="Edad">
-    <input type="text" name="Edad">
-    <input type="text" name="Edad">
-    <input type="text" name="Edad">
-    <input type="text" name="Edad">
-    <input type="text" name="Edad">
-    <input type="text" name="Edad">
-    <input type="text" name="Edad">
-    <input type="text" name="Edad">
+    <input type="text" name="nombre_actividad">
+    <input type="text" name="edad">
+    <input type="text" name="lugar">
+    <input type="text" name="dias">
+    <input type="text" name="grupo1">
+    <input type="text" name="grupo2">
+    <input type="text" name="grupo3">
+    <input type="text" name="grupo4">
+    <input type="text" name="grupo5">
+    <input type="text" name="horario_grupo1">
+    <input type="text" name="horario_grupo2">
+    <input type="text" name="horario_grupo3">
+    <input type="text" name="horario_grupo4">
+    <input type="text" name="horario_grupo5">
+    <input type="text" name="nombre_profesor">
+    <input type="text" name="costo_clase">
+    <input type="text" name="costo_mensual_paquete1">
+    <input type="text" name="costo_mensual_paquete2">
+    <input type="text" name="costo_mensual_paquete3">
+    <input type="text" name="costo_mensual_paquete4">
+    <input type="text" name="costo_mensual_paquete5">
+    <input type="text" name="costo_mensual_paquete6">
+    <input type="text" name="costo_mensual_paquete7">
+    <input type="text" name="costo_mensual_paquete8">
 
     <input type="submit" name="Enviar">
 
